b4f7f1227f00c1b33821807445a6b760c9c09238
Matthew White <mattrjw@gmail.com>
2016-08-28 02:21:26 +1000

    Added static files to project

---
+++ public/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+    $(document).ready(function() {
+    $('select').material_select();
+  });
+})(jQuery); // end of jQuery name space
