f2a5087c35fe09c0ec4067340612a68884b39057
Matthew White <mattrjw@gmail.com>
2016-08-28 14:26:13 +1000

    Extended analysis area

--- public/js/form.js
+++ public/js/form.js
@@ -1,8 +1,24 @@
 $( "#form-two" ).hide();
 $( "#form-three" ).hide();
 $( "#form-four" ).hide();
+$( "#analysis" ).hide();
 
 $("#submit-form-one").click(function() {
  $("#form-one").hide();
  $("#form-two").show();
+});
+
+$("#submit-form-two").click(function() {
+ $("#form-two").hide();
+ $("#form-three").show();
+});
+
+$("#submit-form-three").click(function() {
+ $("#form-three").hide();
+ $("#form-four").show();
+});
+
+$("#submit-form-four").click(function() {
+ $("#form-card").hide();
+ $("#analysis").show();
 });
